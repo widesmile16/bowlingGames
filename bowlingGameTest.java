0f593032d92a2b55f30067d1ac1f38557bf05ad5
widesmile16 <widesmile16@gmail.com>
2015-05-30 16:31:28 +0500

    third

--- bowlingGameTest.java
+++ bowlingGameTest.java
@@ -6,12 +6,12 @@ public class BowlingGameTest extends TestCase {
   protected void setUp() throws Exception {
     g = new Game();
   }
-  
+
   private void rollMany(int n, int pins) {
     for (int i = 0; i < n; i++)
       g.roll(pins);
   }
-  
+
   public void testGutterGame() throws Exception {
     rollMany(20, 0);
     assertEquals(0, g.score());
@@ -20,5 +20,17 @@ public class BowlingGameTest extends TestCase {
   public void testAllOnes() throws Exception {
     rollMany(20,1);
     assertEquals(20, g.score());
+  }
+
+  public void testOneSpare() throws Exception {
+    rollSpare();
+    g.roll(3);
+    rollMany(17,0);
+    assertEquals(16,g.score());
+  }
+
+  private void rollSpare() {
+    g.roll(5);
+    g.roll(5);
   }
 }
